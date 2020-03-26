fab39c6f564c14dfaa2d8c10a4e37557e29e52c0
Michael Anderson <mda2894@gmail.com>
2020-03-26 02:22:01 -0400

    Start project 8 VMTranslator

---
+++ 08/Code.py
@@ -0,0 +1,140 @@
+class Code:
+
+    def __init__(self):
+        self.arithmeticTable = {
+            "add" : "M=D+M",
+            "sub" : "M=M-D",
+            "and" : "M=D&M",
+            "or"  : "M=D|M",
+            "neg" : "M=-M",
+            "not" : "M=!M",
+            "eq"  : "D;JEQ",
+            "gt"  : "D;JGT",
+            "lt"  : "D;JLT"
+        }
+        self.segmentTable = {
+            "local"    : "LCL",
+            "argument" : "ARG",
+            "this"     : "THIS",
+            "that"     : "THAT"
+        }
+        self.init = [
+            "@256",
+            "D=A",
+            "@SP",
+            "M=D",
+            "@Sys.init",
+            "0;JMP"
+        ]
+        self.twoArgCalc = [
+            "@SP",
+            "M=M-1",
+            "A=M",
+            "D=M",
+            "@SP",
+            "M=M-1",
+            "A=M",
+            "cmd",
+            "@SP",
+            "M=M+1"
+        ]
+        self.oneArgCalc = [
+            "@SP",
+            "M=M-1",
+            "A=M",
+            "cmd",
+            "@SP",
+            "M=M+1"
+        ]
+        self.comparison = [
+            "@SP",
+            "M=M-1",
+            "A=M",
+            "D=M",
+            "@SP",
+            "M=M-1",
+            "A=M",
+            "D=M-D",
+            "@JUMP",
+            "cmd",
+            "@SP",
+            "A=M",
+            "M=0",
+            "@EXIT",
+            "0;JMP",
+            "(JUMP)",
+            "@SP",
+            "A=M",
+            "M=-1",
+            "(EXIT)",
+            "@SP",
+            "M=M+1",
+        ]
+        self.pushConstant = [
+            "num",
+            "D=A",
+            "@SP",
+            "A=M",
+            "M=D",
+            "@SP",
+            "M=M+1"
+        ]
+        self.pushLclArgThisThat = [
+            "@ind",
+            "D=A",
+            "@seg",
+            "A=D+M",
+            "D=M",
+            "@SP",
+            "A=M",
+            "M=D",
+            "@SP",
+            "M=M+1"
+        ]
+        self.popLclArgThisThat = [
+            "@ind",
+            "D=A",
+            "@seg",
+            "D=D+M",
+            "@SP",
+            "A=M-1",
+            "D=D+M",
+            "A=D-M",
+            "M=D-A",
+            "@SP",
+            "M=M-1"
+        ]
+        self.pushPointerTemp = [
+            "@address",
+            "D=M",
+            "@SP",
+            "A=M",
+            "M=D",
+            "@SP",
+            "M=M+1"
+        ]
+        self.popPointerTemp = [
+            "@SP",
+            "M=M-1",
+            "A=M",
+            "D=M",
+            "@address",
+            "M=D"
+        ]
+        self.pushStatic = [
+            "@static",
+            "D=M",
+            "@SP",
+            "A=M",
+            "M=D",
+            "@SP",
+            "M=M+1"
+        ]
+        self.popStatic = [
+            "@SP",
+            "M=M-1",
+            "A=M",
+            "D=M",
+            "@static",
+            "M=D"
+        ]
---
+++ 08/VMTranslator.py
@@ -0,0 +1,56 @@
+import Parser as p, CodeWriter as cw, os, sys
+
+def main(dir):
+    vmt = VMTranslator(dir)
+    vmt.translate()
+
+class VMTranslator:
+
+    def __init__(self, dir):
+        self.dir = dir
+        self.code = cw.CodeWriter()
+
+    def translate(self):
+        self.code.writeInit()
+
+        for file in [f for f in os.listdir(self.dir) if f.endswith('.vm')]:
+            filepath = os.path.join(self.dir, file)
+            parser = p.Parser(filepath)
+            parser.readFile()
+            filestub = file.replace(".vm", "")
+
+            for line in parser.lines:
+                type = parser.type(line)
+                arg1 = parser.arg1(line)
+                arg2 = parser.arg2(line)
+                self.code.writeComment(line)
+
+                if type == "ARITHMETIC":
+                    self.code.writeArithmetic(arg1)
+                elif type == "PUSH":
+                    self.code.writePush(arg1, arg2, filestub)
+                elif type == "POP":
+                    self.code.writePop(arg1, arg2, filestub)
+                elif type == "LABEL":
+                    self.code.writeLabel(arg1)
+                elif type == "GOTO":
+                    self.code.writeGoto(arg1)
+                elif type == "IF":
+                    self.code.writeIf(arg1)
+                elif type == "FUNCTION":
+                    self.code.writeFunction(arg1, arg2)
+                elif type == "CALL":
+                    self.code.writeCall(arg1, arg2)
+                elif type == "RETURN":
+                    self.code.writeReturn()
+
+        self.writeFile()
+
+    def writeFile(self):
+        outfile = self.dir + "\\" + self.dir.split("\\")[-1] + ".asm"
+        with open(outfile, "w") as f:
+            for line in self.code.out:
+                f.write(line + "\n")
+
+if __name__ == '__main__':
+    main(sys.argv[1])
